62562e110fc74afe48e65042633502a5ffe29a1a
chnherb <chnhuangbo@qq.com>
2022-05-27 22:54:08 +0800

    feat: hello world

--- ch01-hello/main/hello.go
+++ ch01-hello/main/hello.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
 	// go run hello.go
 	// go build hello.go
 	// ./hello
+
+	// go run hello.go haha
+	fmt.Println(os.Args)
+	if len(os.Args) > 1 {
+		fmt.Println("args:", os.Args[1])
+	}
+	fmt.Println("hello world")
+
+	//os.Exit(-1) // exit status 255
+	os.Exit(-1) // 0 success
+
 }
